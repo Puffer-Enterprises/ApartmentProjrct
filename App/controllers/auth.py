99217420d4dabe49bb678c2720f442eba4c94da7
jeremy87lee <157717264+jeremy87lee@users.noreply.github.com>
2025-04-15 16:59:48 -0400

    fixed login

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -15,16 +15,13 @@ def setup_jwt(app):
   # configure's flask jwt to resolve get_current_identity() to the corresponding user's ID
   @jwt.user_identity_loader
   def user_identity_lookup(identity):
-    user = User.query.filter_by(username=identity).one_or_none()
-    if user:
-        return user.id
-    return None
+    return identity  # it's already a string (username)
 
   @jwt.user_lookup_loader
   def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
-    return User.query.get(identity)
-
+    return User.query.filter_by(username=identity).first()
+  
   return jwt
 
 
@@ -35,10 +32,11 @@ def add_auth_context(app):
       try:
           verify_jwt_in_request()
           user_id = get_jwt_identity()
-          current_user = User.query.get(user_id)
+          print(f"[CTX] JWT detected. User ID: {user_id}")
+          current_user = User.query.filter_by(username=user_id).first()
           is_authenticated = True
       except Exception as e:
-          print(e)
+          print(f"[CTX] JWT error: {e}")
           is_authenticated = False
           current_user = None
       return dict(is_authenticated=is_authenticated, current_user=current_user)
