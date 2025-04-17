03c0c9b1af46cd5891ba818e334b0edceebf2270
jeremy87lee <157717264+jeremy87lee@users.noreply.github.com>
2025-04-16 20:25:16 -0400

    Update auth.py

--- App/views/auth.py
+++ App/views/auth.py
@@ -36,13 +36,14 @@ def login_action():
     if not token:
         flash('Bad username or password given'), 401
     else:
+        response = redirect(url_for('index_views.check'))
         flash('Login Successful')
         set_access_cookies(response, token) 
     return response
 
 @auth_views.route('/logout', methods=['GET'])
 def logout_action():
-    response = redirect(request.referrer) 
+    response = redirect(url_for('index_views.index_page')) 
     flash("Logged Out!")
     unset_jwt_cookies(response)
     return response
@@ -71,3 +72,4 @@ def logout_api():
     response = jsonify(message="Logged Out!")
     unset_jwt_cookies(response)
     return response
+
